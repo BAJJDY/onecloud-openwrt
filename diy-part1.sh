81fa354aa84da439050448eb34f8e374335a7f15
BAJJDY <255525881+BAJJDY@users.noreply.github.com>
2026-08-09 20:55:54 +0800

    feat: ?????.3???????????- OpenClash+nginx+Argon??????+harbor-file

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
@@ -17,5 +10,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-#sed -i '2s/^#//' feeds.conf.default
-#sed -i '3s/^/#/' feeds.conf.default
+# Clone luci-app-harbor-file into package directory
+git clone https://github.com/destan19/luci-app-harbor-file.git package/luci-app-harbor-file
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# OpenWrt DIY script part 2 (After Install feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+# Modify default IP to bypass router LAN IP
+sed -i 's/192.168.1.1/192.168.8.110/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify hostname to 玩客云
+sed -i 's/LEDE/玩客云/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
+# ========== Download OpenClash kernels (armv7) ==========
+mkdir -p files/etc/openclash/core
 
-# NOTE: bash replacement disabled - bash is excluded from .config
-# sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+# Get latest mihomo (Clash.Meta) release URL for armv7
+MIHOMO_LATEST=$(curl -sL https://api.github.com/repos/MetaCubeX/mihomo/releases/latest | grep -oP '"tag_name":\s*"\K[^"]+')
+if [ -n "$MIHOMO_LATEST" ]; then
+    MIHOMO_URL="https://github.com/MetaCubeX/mihomo/releases/download/${MIHOMO_LATEST}/mihomo-linux-armv7-${MIHOMO_LATEST}.gz"
+    curl -L "$MIHOMO_URL" -o /tmp/clash-armv7.gz
+    if [ -f /tmp/clash-armv7.gz ]; then
+        gzip -d -f /tmp/clash-armv7.gz
+        cp /tmp/clash-armv7 files/etc/openclash/core/clash
+        cp /tmp/clash-armv7 files/etc/openclash/core/clash_tun
+        chmod +x files/etc/openclash/core/clash
+        chmod +x files/etc/openclash/core/clash_tun
+        echo "OpenClash kernels downloaded successfully: $MIHOMO_LATEST"
+    else
+        echo "WARNING: Failed to download mihomo kernel"
+    fi
+else
+    echo "WARNING: Failed to get latest mihomo release version, trying direct download"
+    # Fallback to a known stable version
+    curl -L "https://github.com/MetaCubeX/mihomo/releases/download/v1.19.8/mihomo-linux-armv7-v1.19.8.gz" -o /tmp/clash-armv7.gz
+    if [ -f /tmp/clash-armv7.gz ]; then
+        gzip -d -f /tmp/clash-armv7.gz
+        cp /tmp/clash-armv7 files/etc/openclash/core/clash
+        cp /tmp/clash-armv7 files/etc/openclash/core/clash_tun
+        chmod +x files/etc/openclash/core/clash
+        chmod +x files/etc/openclash/core/clash_tun
+        echo "OpenClash kernels downloaded (fallback v1.19.8)"
+    fi
+fi
+
+# ========== Set executable permissions for scripts ==========
+chmod +x files/etc/rc.local 2>/dev/null
+chmod +x files/etc/uci-defaults/99-openclash-settings 2>/dev/null
+chmod +x files/etc/uci-defaults/99-harbor-settings 2>/dev/null
+chmod +x files/etc/firewall.user 2>/dev/null
